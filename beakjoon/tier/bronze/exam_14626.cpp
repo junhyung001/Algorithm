5ce372538a6fb273d9f96afa79d895d83ae4286b
조준형 <junhyeong@jojunhyeong-ui-MacBookPro.local>
2025-06-24 14:31:09 +0900

    Add : 2025.06.24

--- beakjoon/tier/bronze/exam_14626.cpp
+++ beakjoon/tier/bronze/exam_14626.cpp
@@ -7,12 +7,12 @@ int main(){
     string str;
     int sum=0;
     cin>>str;
-    int location = str.find("*");
+    int idx = str.find("*");
     
     for (int i = 0; i < 10; i++)
     {
         sum=0;
-        str[location] = i+'0';
+        str[idx] = i+'0';
         for(int j=0; j<13; j++){
             if(j%2==1){
                 sum += (str[j]-'0')*3;
@@ -21,9 +21,8 @@ int main(){
             sum += (str[j]-'0');
         }
         if(sum%10==0){
-            cout << str[location];
+            cout << str[idx];
             return 0;
         }
     }
-    
 }

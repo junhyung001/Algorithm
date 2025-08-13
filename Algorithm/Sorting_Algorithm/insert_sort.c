b5f1d664dddefd654ac5485b0a53be3587b630bc
조준형 <junhyung8325@gmail.com>
2025-08-13 09:51:42 +0900

    Add:2025.08.12

--- Algorithm/Sorting_Algorithm/insert_sort.c
+++ Algorithm/Sorting_Algorithm/insert_sort.c
@@ -5,7 +5,7 @@ int main(){
     int arr[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
     for (int i = 0; i < 9; i++) {
         j=i;
-        while (arr[j]>arr[j+1]) {
+        while (j >= 0 && arr[j]>arr[j+1]) {
             temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;

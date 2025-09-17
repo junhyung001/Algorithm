0e59d23018bfd29f9a3c214a4fe3e807c3ab848b
조준형 <junhyung8325@gmail.com>
2025-09-17 12:31:45 +0900

    Add : 2025.09.17

---
+++ beakjoon/tier/bronze/9086. 문자열/exam_9086.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    string str;
+
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> str;
+        cout << str[0] << str[str.size()-1] << endl;
+    }
+    
+}

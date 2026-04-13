86b6727169d305081bf9adef04e61770453d95fd
JunHyeong <84774432+junhyung001@users.noreply.github.com>
2026-04-13 18:48:44 +0900

    [Silver V] Title: 칠무해, Time: 6196 ms, Memory: 76544 KB -BaekjoonHub

---
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n;
+
+    vector<double> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    cout << fixed << setprecision(3);
+    for (int i = 0; i < 7; i++) {
+        cout << a[i] << "\n";
+    }
+    
+}
